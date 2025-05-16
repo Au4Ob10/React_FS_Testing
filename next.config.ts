7b7490a2a6112d06d4177754a6033627352f6af0
au4ob10 <agranchelli27@gmail.com>
2025-05-16 13:00:44 -0400

    working mp hand detection

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+   webpack(config) {
+    config.module.rules.push({
+      test: /\.task$/,
+      type: "asset/resource",
+      generator: {
+        filename: "static/media/[name][ext]",
+      },
+    })
   reactStrictMode: false
-};
+  return config
+}};
 
 export default nextConfig;
