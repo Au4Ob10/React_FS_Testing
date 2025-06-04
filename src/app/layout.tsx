9b2aa84ba04136941cc70e922cb21c32423e7f05
AGran <agranchelli27@gmail.com>
2025-06-04 15:52:51 -0400

    Revert "working chakraprovider"
    This reverts commit 2ef4243b07d923b92519bb00de0e74ebf7b52fc1.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from 'react';
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      {children}
+        {children}
       </body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -16,9 +16,8 @@ import React, { useEffect, useRef, useState } from "react";
 import { FilesetResolver, HandLandmarker} from "@mediapipe/tasks-vision";
 import * as fp from "fingerpose"
 import fsl_gestures from "../../components/generateSigns"
-import Webcam from "react-webcam";
 
-import { RiCameraFill, RiCameraOffFill } from "react-icons/ri"
+
 
 import {
   Text,
@@ -46,7 +45,6 @@ const Demo = () => {
     const [currHandCoord, setCurrHandCoord] = useState({});
     const [coordDeltaVals, setCoordDeltaVals] = useState({});
     const [stateTest, setstateTest] = useState({})
-     const [camState, setCamState] = useState("on")
     
 
 
@@ -251,9 +249,7 @@ const Demo = () => {
             // console.log(latestDetection.worldLandmarks[0].slice(7,9).slice(0,2))
         
     
-const turnOffCamera = () => {
-    console.log("test")
-}
+
     
     return (
    
@@ -285,12 +281,41 @@ const turnOffCamera = () => {
 
           <Box id="webcam-container">
             {camState === "on" ? (
-              <Webcam id="webcam" ref={videoRef} style={{transform: "scaleX(-1)"}} />
+              <Webcam id="webcam" ref={webcamRef} style={{transform: "scaleX(-1)"}} />
             ) : (
               <div id="webcam" background="black"></div>
             )}
 
-           
+            {sign ? (
+              <div
+                style={{
+                  position: "absolute",
+                  marginLeft: "auto",
+                  marginRight: "auto",
+                  right: "calc(50% - 50px)",
+                  bottom: 100,
+                  textAlign: "-webkit-center",
+                }}
+              >
+                <Text color="white" fontSize="sm" mb={1}>
+                  detected gestures
+                </Text>
+                <Text>{messageBody}</Text>
+                <img
+                  alt="signImage"
+                  src={
+                    Signimage[sign]?.src
+                      ? Signimage[sign].src
+                      : "/loveyou_emoji.svg"
+                  }
+                  style={{
+                    height: 30,
+                  }}
+                />
+              </div>
+            ) : (
+              " "
+            )}
           </Box>
 
           <canvas id="gesture-canvas" ref={canvasRef} style={{}} />
@@ -334,37 +359,6 @@ const turnOffCamera = () => {
 
 export default Demo;
 
-
-//  {sign ? (
-//               <div
-//                 style={{
-//                   position: "absolute",
-//                   marginLeft: "auto",
-//                   marginRight: "auto",
-//                   right: "calc(50% - 50px)",
-//                   bottom: 100,
-//                   textAlign: "-webkit-center",
-//                 }}
-//               >
-//                 <Text color="white" fontSize="sm" mb={1}>
-//                   detected gestures
-//                 </Text>
-//                 <Text>{messageBody}</Text>
-//                 <img
-//                   alt="signImage"
-//                   src={
-//                     Signimage[sign]?.src
-//                       ? Signimage[sign].src
-//                       : "/loveyou_emoji.svg"
-//                   }
-//                   style={{
-//                     height: 30,
-//                   }}
-//                 />
-//               </div>
-//             ) : (
-//               " "
-//             )}
 //  <Button onClick={() => gestureFunc()}>title</Button>
     //  <>
     //     <h1>Is there a Hand? {handPresence ? "Yes" : "No"}</h1>
