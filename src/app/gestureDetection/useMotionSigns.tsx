9a520f19f57400c21504f7a503093e4b7899a0fa
AGran <agranchelli27@gmail.com>
2025-07-17 22:05:12 -0400

    old motion signs file

--- src/app/gestureDetection/useMotionSigns.tsx
+++ src/app/gestureDetection/useMotionSigns.tsx
@@ -5,8 +5,25 @@ import { messageContext } from '../messageContext';
 const useMotionSigns = (pinkyRef, indexFingerRef) => {
   const pinkyTipArr = useRef([]);
   const indexTipArr = useRef([]);
+  const gestureStep = useRef(null);
+
+  console.log(indexFingerRef.current);
 
   const { setMessageBody } = useContext(messageContext);
+
+  const rafInterval = (callback, interval) => {
+    let start = performance.now();
+    const loop = (now) => {
+      if (now - start >= interval) {
+        callback();
+        start = now;
+      }
+
+      requestAnimationFrame(loop);
+    };
+
+    requestAnimationFrame(loop);
+  };
 
   const noop = () => {};
 
@@ -87,43 +104,57 @@ const useMotionSigns = (pinkyRef, indexFingerRef) => {
         return val >= min && val <= max;
       };
 
-      let i = 0;
-
-      const rangeVals = Object.values(motionSigns.Z)[i];
       const zPoints = Object.keys(motionSigns.Z);
 
-      const xFloor = rangeVals.xFloor;
-      const xCeil = rangeVals.xCeil;
-      const yFloor = rangeVals.yFloor;
-      const yCeil = rangeVals.yCeil;
+      //   const indexFingerX: any = indexTipArr.current.at(-1).x;
+      //   const indexFingerY = indexTipArr.current.at(-1).y;
 
-      let isWaiting = false;
+      const xMidpoint = 0.31;
+      const yMidpoint = 0.75;
+
+      const gesturePointNums = ['point1', 'point2', 'point3', 'point4'];
 
       const createZ = () => {
         if (indexFingerRef.current) {
           indexTipArr.current.push(indexFingerRef.current);
 
           // z vals
-          const indexFingerX: any = indexTipArr.current.at(-1).x;
-          const indexFingerY = indexTipArr.current.at(-1).y;
+          //   const indexFingerX: any = indexTipArr.current.at(-1).x;
+          //   const indexFingerY = indexTipArr.current.at(-1).y;
 
-          if (i < zPoints.length - 1) {
+          const indexFingerX = indexFingerRef.current.x;
+          const indexFingerY = indexFingerRef.current.y;
+
+          //   console.log("x val:",indexFingerX, "\n", "y val:", indexFingerY )
+          let i = 0;
+          let isWaiting = false;
+
+          if (i < zPoints.length) {
+            const currentPoint = motionSigns.Z[zPoints[i]];
+
+            const xFloor = currentPoint.xFloor;
+            const xCeil = currentPoint.xCeil;
+            const yFloor = currentPoint.yFloor;
+            const yCeil = currentPoint.yCeil;
+
             if (
               coordRange(indexFingerX, xFloor, xCeil) &&
               coordRange(indexFingerY, yFloor, yCeil) &&
               !isWaiting
             ) {
               isWaiting = true;
+              console.log();
 
               setTimeout(() => {
                 console.log(zPoints[i]);
                 i++;
 
-                if (i === 4) {
+                if (i === zPoints.length) {
                   setMessageBody((msg) => msg + 'Z');
+                  i = 0;
                 }
                 isWaiting = false;
-              }, 300);
+              }, 100);
             }
           }
         }
